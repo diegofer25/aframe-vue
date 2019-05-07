62b697fa088e0ed8a6981aa27daf7e80fbd57ff9
Diego Lamarão <diego.lamarao92@gmail.com>
2019-05-07 14:04:09 -0300

    added aframe

---
+++ src/components/main-scene.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="main-scene">
+    <a-scene>
+      <a-box
+        color="red"
+        position="0 2 -5"
+        rotation="0 45 45"
+        scale="2 2 2"
+      ></a-box>
+      <a-entity environment="preset: forest; dressingAmount: 500"></a-entity>
+    </a-scene>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "main-scene"
+};
+</script>
+
+<style scoped lang="scss"></style>
